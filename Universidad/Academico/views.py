49bb887f6d572e9ed8989ad254ecd5b2831189ae
Guillermo Ismodes Lopez <gismodes37@gmail.com>
2022-12-26 17:49:15 -0300

    first commit

---
+++ Universidad/Academico/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render, redirect
+from .models import Curso
+from django.contrib import messages
+
+# Create your views here.
+
+def home(request):
+    cursosListados = Curso.objects.all()
+    messages.success(request, '¡Cursos Listados de forma correcta..!')
+    return render(request, "gestionCursos.html", {"cursos": cursosListados})
+
+def registrarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['txtCreditos']
+    
+    curso = Curso.objects.create(codigo=codigo, nombre=nombre, creditos=creditos)
+    messages.success(request, '¡Cursos Registrado de forma correcta..!')    
+    return redirect('/')
+
+def edicionCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    return render(request, "edicionCurso.html", {"curso": curso})
+
+def editarCurso(request):
+    codigo = request.POST['txtCodigo']
+    nombre = request.POST['txtNombre']
+    creditos = request.POST['txtCreditos']  
+    
+    curso = Curso.objects.get(codigo=codigo)
+    curso.nombre = nombre
+    curso.creditos = creditos
+    curso.save()
+    messages.success(request, '¡Cursos Modificado de forma correcta..!')
+    return redirect('/')
+
+def eliminarCurso(request, codigo):
+    curso = Curso.objects.get(codigo=codigo)
+    curso.delete()
+    messages.success(request, '¡Cursos Eliminado de forma correcta..!')
+    return redirect('/')
+
+
+
